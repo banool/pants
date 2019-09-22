69d87ebfb3aff91a14fcdd878afa0a762e8f4f32
Daniel Porteous <dport@fb.com>
2019-09-22 11:42:00 -0700

    Display message on index page if add was successful

--- src/add.rs
+++ src/add.rs
@@ -1,13 +1,23 @@
-use rocket::request::{FromForm,Form};
+extern crate rocket_contrib;
+extern crate serde_json;
+
+use rocket::request::{Form, FromForm};
+use rocket::response::{Flash, Redirect};
+
+#[derive(Serialize)]
+struct TemplateContext {
+    url: String,
+}
 
 #[derive(FromForm)]
 pub struct PocketAddForm {
     title: String,
-    tags: String,  // Comma separated.
+    tags: String, // Comma separated.
 }
 
-#[post("/add", data = "<pocket_add_form>")]
-pub fn add(pocket_add_form: Option<Form<PocketAddForm>>) -> String {
-    let url: String = format!("https://dport.me/pages/{}", "hey");
-    format!("Created and added {} to Pocket successfully", url)
+#[post("/add", data = "<form>")]
+pub fn add(form: Form<PocketAddForm>) -> Flash<Redirect> {
+    assert_eq!(form.title.is_empty(), false);
+    let url: String = format!("https://dport.me/pages/{}", form.title);
+    Flash::success(Redirect::to("/"), url)
 }
--- src/index.rs
+++ src/index.rs
@@ -1,13 +1,21 @@
 extern crate rocket_contrib;
 extern crate serde_json;
 
+use rocket::request::FlashMessage;
 use rocket_contrib::templates::Template;
 
 #[derive(Serialize)]
-struct TemplateContext {}
+struct TemplateContext {
+    added: Option<String>,
+}
 
 #[get("/")]
-pub fn index() -> Template {
-    let context = TemplateContext {};
+pub fn index(flash: Option<FlashMessage>) -> Template {
+    let added: Option<String>;
+    match flash {
+        Some(flash_message) => added= Some(flash_message.msg().to_string()),
+        None => added = None,
+    }
+    let context = TemplateContext { added: added };
     Template::render("index", &context)
 }
