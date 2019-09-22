b2bd3a03f93539d5ab8061c8baab437041fc009d
Daniel Porteous <dport@fb.com>
2019-09-22 12:56:00 -0700

    Get file creation working

--- src/add.rs
+++ src/add.rs
@@ -3,6 +3,9 @@ extern crate serde_json;
 
 use rocket::request::{Form, FromForm};
 use rocket::response::{Flash, Redirect};
+use std::env;
+use std::fs::File;
+use std::path::PathBuf;
 
 #[derive(Serialize)]
 struct TemplateContext {
@@ -18,6 +21,26 @@ pub struct PocketAddForm {
 #[post("/add", data = "<form>")]
 pub fn add(form: Form<PocketAddForm>) -> Flash<Redirect> {
     assert_eq!(form.title.is_empty(), false);
-    let url: String = format!("https://dport.me/pages/{}", form.title);
+
+    let title: String = form
+        .title
+        .chars()
+        .filter(|&c| c.is_alphanumeric())
+        .collect();
+    let title = format!("{}.html", title);
+
+    let created_path = create_page_file(&title);
+
+    let url = format!("{}/{}", &env::var("PANTS_SITE").unwrap(), title);
+
     Flash::success(Redirect::to("/"), url)
+}
+
+fn create_page_file(title: &str) -> String {
+    // Creates a file, returns the path of the file it made.
+    let mut path = PathBuf::new();
+    path.push(&env::var("PANTS_PAGES_ROOT").unwrap());
+    path.push(title);
+    File::create(path.as_path()).unwrap();
+    path.as_path().display().to_string()
 }
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,20 @@ use rocket_contrib::templates::Template;
 mod add;
 mod index;
 
+use std::env;
+use std::fs::File;
+
+const REQUIRED_ENV_VARS: &'static [&'static str] = &["PANTS_PAGES_ROOT", "PANTS_SITE"];
+
 fn main() {
+    for s in REQUIRED_ENV_VARS.iter() {
+        if env::var(s).is_err() {
+            panic!(format!("This env var must be set: {}", s));
+        }
+    }
+    let pages_root = &env::var("PANTS_PAGES_ROOT").unwrap();
+    File::open(pages_root).expect(format!("The path {} doesn't exist", pages_root).as_str());
+
     rocket::ignite()
         .mount("/", routes![index::index])
         .mount("/", routes![add::add])
